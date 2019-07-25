ea91364ab639021702418f74c463f776d4779f34
arslansiddique <arsi.555@gmail.com>
2019-07-25 18:11:52 +0500

    Bug in Comments Controller

--- src/Controllers/CommentsController.php
+++ src/Controllers/CommentsController.php
@@ -53,7 +53,7 @@ class CommentsController extends Controller
         $comment->setPurifiedContent($request->get('content'));
 
         $comment->ticket_id = $request->get('ticket_id');
-        $comment->user_id = \Sentinel::getusser()->id;
+        $comment->user_id = \Sentinel::getuser()->id;
         $comment->save();
 
         $ticket = Models\Ticket::find($comment->ticket_id);
