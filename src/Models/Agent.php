a601a48f07761493ee1db9b06b6880100a0e7cc6
arslansiddique <arsi.555@gmail.com>
2020-08-30 03:44:23 +0500

    Agent Model changes to adjust Agent name in dropdown menu.

--- src/Models/Agent.php
+++ src/Models/Agent.php
@@ -79,9 +79,9 @@ class Agent extends User
     public function scopeAgentsLists($query)
     {
         if (version_compare(app()->version(), '5.2.0', '>=')) {
-            return $query->where('ticketit_agent', '1')->pluck('name', 'id')->toArray();
+            return $query->where('ticketit_agent', '1')->get()->pluck('full_name', 'id')->toArray();
         } else { // if Laravel 5.1
-            return $query->where('ticketit_agent', '1')->lists('name', 'id')->toArray();
+            return $query->where('ticketit_agent', '1')->get()->pluck('full_name', 'id')->toArray();
         }
     }
 
@@ -104,6 +104,7 @@ class Agent extends User
             if (Sentinel::getUser()->ticketit_agent) {
                 return true;
             }
+            return false;
         }
     }
 
