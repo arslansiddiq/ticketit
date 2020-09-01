9a14ad8d51c28f7d2694206d43978f124eb6bb08
arslansiddique <arsi.555@gmail.com>
2020-09-01 16:26:01 +0500

    Add images / links in email tamplete as links. Add seperate lines for zapier template

--- src/Views/bootstrap3/emails/assigned.blade.php
+++ src/Views/bootstrap3/emails/assigned.blade.php
@@ -1,5 +1,6 @@
 <?php $notification_owner = unserialize($notification_owner);?>
 <?php $ticket = unserialize($ticket);?>
+<?php $images = unserialize($images);?>
 
 @extends('ticketit::emails.templates.leadgenerated')
 
@@ -30,42 +31,57 @@
 	]) !!} --}}
 
 	<h3 class="email-template__intro" style="margin: 0 0 20px;font-weight: 400;font-size: 20px;">Hello Support Team,</h3>
-	<p class="email-template__message" style="line-height: 1.4em;font-size: 15px;">New ticket has been created in Lead Generated support system.</p>
+	<p class="email-template__message" style="line-height: 1.4em;font-size: 15px;">
+		A new ticket has been created in the Lead Generated support system.
+	</p>
 	<table class="email-template__schedule" style="margin: 50px auto;width: 100%;">
 	  <tbody>
 	    <tr>
-	      <td style="padding: 5px 10px 5px 0;width: 100px;"><span class="heading-bold" style="font-weight: 600;">Ticket #:</span></td>
+	      <td style="padding: 5px 10px 5px 0;width: 100px; vertical-align:top;"><span class="heading-bold" style="font-weight: 600;">Ticket #:</span></td>
 	      <td> {{ $ticket->id }} </td>
 	    </tr>
 	    <tr>
-	      <td style="padding: 5px 10px 5px 0;width: 100px;"><span class="heading-bold" style="font-weight: 600;">Date:</span></td>
+	      <td style="padding: 5px 10px 5px 0;width: 100px; vertical-align:top;"><span class="heading-bold" style="font-weight: 600;">Date:</span></td>
 	      <td> {{ $ticket->created_at->toDayDateTimeString() }} </td>
 	    </tr>
 	    <tr>
-	      <td style="padding: 5px 10px 5px 0;width: 100px;"><span class="heading-bold" style="font-weight: 600;">Title:</span></td>
+	      <td style="padding: 5px 10px 5px 0;width: 100px; vertical-align:top;"><span class="heading-bold" style="font-weight: 600;">Title:</span></td>
 	      <td> {{ $ticket->subject }} </td>
 	    </tr>
 	    <tr>
-	      <td style="padding: 5px 10px 5px 0;width: 100px;"><span class="heading-bold" style="font-weight: 600;">User:</span></td>
+	      <td style="padding: 5px 10px 5px 0;width: 100px; vertical-align:top;"><span class="heading-bold" style="font-weight: 600;">User:</span></td>
 	      <td> {{ $notification_owner->name }} </td>
 	    </tr>
 	    <tr>
-	      <td style="padding: 5px 10px 5px 0;width: 100px;"><span class="heading-bold" style="font-weight: 600;">Status:</span></td>
+	      <td style="padding: 5px 10px 5px 0;width: 100px; vertical-align:top;"><span class="heading-bold" style="font-weight: 600;">Status:</span></td>
 	      <td> {{ $ticket->status->name }} </td>
 	    </tr>
 	    <tr>
-	      <td style="padding: 5px 10px 5px 0;width: 100px;"><span class="heading-bold" style="font-weight: 600;">Category:</span></td>
+	      <td style="padding: 5px 10px 5px 0;width: 100px; vertical-align:top;"><span class="heading-bold" style="font-weight: 600;">Category:</span></td>
 	      <td> {!! ($ticket->category->parent_category) ? $ticket->category->parent_category->name : $ticket->category->name !!} </td>
 	    </tr>
+	    @if ($ticket->category->parent_category)
+		    <tr>
+		      <td style="padding: 5px 10px 5px 0;width: 100px; vertical-align:top;"><span class="heading-bold" style="font-weight: 600;">Sub Category: </span></td>
+		      <td colspan="2"> {!! ($ticket->category->parent_category) ? $ticket->category->name : '-' !!} </td>
+		    </tr>
+	    @endif
 	    <tr>
-	      <td style="padding: 5px 10px 5px 0;width: 100px;"><span class="heading-bold" style="font-weight: 600;">Sub Category: </span></td>
-	      <td> {!! ($ticket->category->parent_category) ? $ticket->category->name : '-' !!} </td>
+	      <td style="padding: 5px 10px 5px 0;width: 100px; vertical-align:top;"><span class="heading-bold" style="font-weight: 600;">Images/Links: </span></td>
+	      <td> 
+	      	@foreach ($images as $image)
+	      		<a href="{{ $image['href'] }}">{{ $image['text'] }}</a> 
+	      	@endforeach
+	      </td>
 	    </tr>
 	    <tr>
-	      <td colspan="2" style="padding: 20px 10px 5px 0;width: 100px;"><span class="heading-bold" style="font-weight: 600;">Description: </span></td>
+	      <td style="padding: 5px 10px 5px 0;width: 100px; vertical-align:top;"><span class="heading-bold" style="font-weight: 600;">Ticket Url: </span></td>
+	      <td> <a href="{{ route($setting->grab('main_route').'.show', $ticket->id) }}">{{ route($setting->grab('main_route').'.show', $ticket->id) }}</a>
+	      </td>
 	    </tr>
 	    <tr>
-	      <td colspan="2"> {!! $ticket->email_content !!} </td>
+	      <td style="padding: 20px 10px 5px 0;width: 100px; vertical-align:top;"><span class="heading-bold" style="font-weight: 600;">Description: </span></td>
+	      <td> {!! $ticket->email_content !!} </td>
 	    </tr>
 	  </tbody>
 	</table>
